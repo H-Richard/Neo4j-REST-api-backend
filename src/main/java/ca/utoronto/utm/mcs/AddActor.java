8362074fbc53a1189c2353c0dd6c3175ebf21fcb
Richard Hong <r.hong@protonmail.com>
2019-10-02 19:33:19 -0400

    implemented AddActor

---
+++ src/main/java/ca/utoronto/utm/mcs/AddActor.java
@@ -0,0 +1,54 @@
+package ca.utoronto.utm.mcs;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.json.*;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.Session;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+
+public class AddActor implements HttpHandler{
+  
+  Driver driver;
+  
+  public AddActor(Driver driver) {
+    this.driver = driver;
+  }
+  
+  
+  public void handle(HttpExchange exchange) {
+    try {
+      if (exchange.getRequestMethod().equals("GET")) {
+          //handleGet(exchange);
+      }
+      else if (exchange.getRequestMethod().equals("PUT")) {
+          handlePut(exchange);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+  
+  public void handlePut(HttpExchange exchange) throws IOException, JSONException {
+    String body = Utils.convert(exchange.getRequestBody());
+    JSONObject deseralized = new JSONObject(body);
+    try {
+      String name = deseralized.getString("name");
+      String actorId = deseralized.getString("actorId");
+      addActor(name, actorId, exchange);
+      exchange.sendResponseHeaders(200, 0);
+    } catch (Exception e) {
+      exchange.sendResponseHeaders(400, 0);
+      e.printStackTrace();
+    }
+  }
+  
+  public void addActor(String name, String actorID, HttpExchange exchange) {
+    try (Session session = driver.session()) {
+      session.run(String.format("CREATE (:actor {name: \"%s\", actorId: \"%s\"})", name, actorID));
+    }
+  }
+}
--- src/main/java/ca/utoronto/utm/mcs/App.java
+++ src/main/java/ca/utoronto/utm/mcs/App.java
@@ -2,14 +2,24 @@ package ca.utoronto.utm.mcs;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
 import com.sun.net.httpserver.HttpServer;
 
 public class App 
 {
     static int PORT = 8080;
     public static void main(String[] args) throws IOException
-    {
+    {   
+        Driver driver = GraphDatabase.driver("bolt://localhost:7687", 
+                                            AuthTokens.basic("neo4j", "password"));
+        
         HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
+        server.createContext("/api/v1/addActor", new AddActor(driver));
+        
         server.start();
         System.out.printf("Server started on port %d...\n", PORT);
     }
